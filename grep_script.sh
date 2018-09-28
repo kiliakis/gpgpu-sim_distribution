45cb70f941d386c507bae960ad5b36914a3ed462
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2018-09-28 17:48:16 +0000

    added design notes for effectively implementing the dpus

--- grep_script.sh
+++ grep_script.sh
@@ -3,7 +3,7 @@ grep "$1" --color -rInH  \
 --exclude-dir="doc" \
 --exclude-dir="\*.git\*" \
 --exclude="*.txt" \
---exclude="cscope.out" \
+--exclude="cscope.*" \
 --exclude-dir="build" \
 --exclude-dir="bin" \
 $2
